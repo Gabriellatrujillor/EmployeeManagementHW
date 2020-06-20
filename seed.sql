ef14377c66cd6f3226ca283a17387db69dcefe34
gabriellatrujillor <gabriella.trujillor@gmail.com>
2020-06-20 10:21:10 -0600

    names

--- seed.sql
+++ seed.sql
@@ -3,24 +3,24 @@ USE employee_tracker;
 
 -- dept table 
 INSERT INTO employee_department(dept_name) 
-VALUES ("LOTR travelling party"),("Adventurer"), ("Hobbit");
+VALUES ("Wizard"),("Adventurer"), ("Hobbit");
 
 
 -- role table
 
 INSERT INTO employee_role (title,salary,employee_department_id)
-VALUES ("Gandlf the Grey",1000000,1),
-("Saruman the White", 500000,2)("Bilbo Baggins",370000,3), ("Merry & Pippin", 500000,3)
+VALUES ("Gandalf the Grey",200000,1),
+("Legolas Greenleaf", 500000,2),("Bilbo Baggins",8500,3), ("Sam Wise", 350,3),
 
 
 
 
 
--- employee table
+-- employee table -- inserting the employees and assigning them to roles
+
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
--- inserting the employees and assigning them to roles
-VALUES ("Bilbo", "Baggins", 1, null),
- ("Dan Katz", "Big Cat", 2,1), ("Eric Sollenberger", "PFT Commenter", 3,1), ("Alexandra Cooper", null,4,1), ("Erika Nardini", null, 5,1);
+VALUES ("Gandalf", "The Gray", 1, 1),
+ ("Legolas", "Greenleaf",2,2), ("Bilbo", "Baggins" 3,3), ("Sam","Wise",3,1), ("Erika Nardini", null, 5,1);
 
 
 --- this is the role of employees, as in wizard, adventurer, elf
